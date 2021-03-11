a954aa0ba9ce3c88b05c8655f50da69391e0202d
Daniel <30556447+danielweber90@users.noreply.github.com>
2021-03-11 16:15:32 +0100

    Update IERC20.sol

--- IERC20.sol
+++ IERC20.sol
@@ -1,12 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.8.0;
 
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
 interface IERC20{
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
     function totalSupply() external view returns (uint);
+    
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
     function balanceOf(address tokenOwner) external view returns (uint balance);
+    
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
     function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
     function transfer(address to, uint tokens) external returns (bool success);
+    
+    **
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits an {Approval} event.
+     */
     function approve(address spender, uint tokens) external returns (bool success);
+    
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
     function transferFrom (address from, address to, uint tokens) external returns (bool success);
     
     event Transfer(address indexed from, address indexed to, uint tokens);
